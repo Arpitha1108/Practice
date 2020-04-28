f29adbd4aa73bf4db5c73600af9d697f6a79c1fd
GANESH <GANESH@Lenovo-PC>
2020-04-28 18:20:59 +0530

    ConcatOperator

---
+++ src/Javabasics/Concatenation.java
@@ -0,0 +1,14 @@
+package Javabasics;
+
+public class Concatenation {
+
+	public static void main(String[] args) {
+		String s1 = "Arpitha";
+		String s2 = " K.R.";
+		String s3=s1+s2;
+		System.out.println(s3);
+		int a=11,b=22,c=33;
+		System.out.println(s3+a+b+c);
+	}
+
+}
